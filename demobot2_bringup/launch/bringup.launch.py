81083f6b531d3f685f7f588de8f51efa1aa74830
neo2 <neo2@box>
2025-12-19 15:20:21 +0800

    add load map config

--- demobot2_bringup/launch/bringup.launch.py
+++ demobot2_bringup/launch/bringup.launch.py
@@ -4,7 +4,7 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.actions import IncludeLaunchDescription
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.substitutions import LaunchConfiguration
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
@@ -16,14 +16,20 @@ def generate_launch_description():
     
     rviz_config_dir = os.path.join(get_package_share_directory('demobot2_bringup'), 'rviz', 'bringup.rviz')
     world_file_dir = os.path.join(get_package_share_directory('demobot2_gazebo'), 'worlds', 'square.world')
+    map_file_dir = os.path.join(get_package_share_directory('demobot2_bringup'), 'maps', 'square.yaml')
     nav2_params_file_dir = os.path.join(get_package_share_directory('demobot2_bringup'), 'params', 'nav2_params.yaml')
 
     params = [
         ('robot_type', 'demobot', 'Type of robot to load in gazebo (demobot, circularbot, cubicbot)'),
         ('gui', 'true', 'Start Gazebo with GUI'),
         ('world', world_file_dir, 'World file to load in Gazebo'),
-        ('slam', 'true', 'Run slam toolbox'),
+        ('map', map_file_dir, 'map file to load in AMCL'),
+        ('slam', 'false', 'Run slam toolbox'),
+        ('localization', 'true', 'Run amcl and map_server'),
+        ('params_file', nav2_params_file_dir, 'navigation parameters file'),
         ('use_sim_time', 'true', 'Use Gazebo simulation time'),
+        ('namespace', '', 'Top-level namespace'),
+        ('autostart', 'true', 'Automatically startup the nav2 stack'),
     ]
     
     for name, default, desc in params:
@@ -42,7 +48,19 @@ def generate_launch_description():
         launch_arguments={
             # 由于navigation_launch中在给params_file默认值前,调用了RewrittenYaml,所以需要显示传入params_file,防止RewrittenYaml找不到文件报错
             'use_sim_time': LaunchConfiguration('use_sim_time'),
-            'params_file': nav2_params_file_dir,
+            'autostart': LaunchConfiguration('autostart'),
+            'params_file': LaunchConfiguration('params_file'),
+        }.items()
+    )
+
+    localization_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([FindPackageShare('nav2_bringup'), '/launch', '/localization_launch.py']),
+        condition=IfCondition(LaunchConfiguration('localization')),
+        launch_arguments={
+            'use_sim_time': LaunchConfiguration('use_sim_time'),
+            'params_file': LaunchConfiguration('params_file'),
+            'autostart': LaunchConfiguration('autostart'),
+            'map': LaunchConfiguration('map'),
         }.items()
     )
 
@@ -62,7 +80,8 @@ def generate_launch_description():
 
     ld.add_action(gazebo_launch)
     ld.add_action(navigation_launch)
+    ld.add_action(localization_launch)
     ld.add_action(slam_launch)
     ld.add_action(rviz_node)
 
-    return ld
+    return ld 
